cf445884c9da2a394aa144399911bf6abc62fcfd
Pablo <pablo.moreiramora@gmail.com>
2011-02-11 19:32:33 -0300

    Create project yamamusic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yamamusic::Application.config.secret_token = '098900d53b04864dcd30dc3ac49f966ea59dba009629a27ef58960bdd388392b8b74429b3122cdef9a013af9d2e5c1ba240baacabee84ae58824dda8a6e63f0f'
