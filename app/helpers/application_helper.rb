121fc17530c7b2c9a110b9068a125adc0866c881
Pablo <pablo.moreiramora@gmail.com>
2011-03-05 18:58:43 -0300

    Add stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def active(name)
+    (controller_name == name) ? 'current' : 'select'
+  end
+
+  def link_to_new_resource
+    if new_action_defined?
+      link_to("New #{resource_class.name.titleize}", new_resource_url)
+    end
+  end
+
+  def new_action_defined?
+    controller.class.action_methods.include?("new")
+  end
+
 end
